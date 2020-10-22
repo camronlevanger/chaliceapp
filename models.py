777ba3bd2ed5548b326fee491ec0c5eea01cbbe0
Camron G. Levanger <camron@mainframe.com>
2020-10-22 15:47:03 -0800

    fixing association

--- models.py
+++ models.py
@@ -5,20 +5,11 @@ from sqlalchemy.orm import relationship
 
 from base import Session, engine, Base
 
-user_teams = Table('userteams', Base.metadata,
-    Column('team_id', Integer, ForeignKey('teams.id')),
-    Column('user_id', Integer, ForeignKey('users.id')),
-    Column('position_id', Integer, ForeignKey('positions.id')),
-    UniqueConstraint('team_id', 'user_id', "position_id", name='uix_team_user_pos')
-)
-
-Index('idxteamposuser', user_teams.c.team_id, user_teams.c.user_id, user_teams.c.position_id , unique=True)
-
 class UserTeams(Base):
     __tablename__ = 'user_teams'
-    __table_args__ = (Index('idxteamposuser', user_teams.c.team_id, user_teams.c.user_id, user_teams.c.position , unique=True) )
-    team_id = Column(Integer, ForeignKey('left.id'), primary_key=True)
-    user_id = Column(Integer, ForeignKey('right.id'), primary_key=True)
+    __table_args__ = (Index('idxteamposuser', team_id, user_id, position , unique=True) )
+    team_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('teams.id'), primary_key=True)
     position = Column(String(50))
     active = Column(Boolean, default=True)
     child = relationship("Team", back_populates="users")
