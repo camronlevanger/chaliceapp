16df92e6239d0799b6597ccbee7d62516c5b71e4
Camron G. Levanger <camron@mainframe.com>
2020-10-22 16:39:09 -0800

    starting routes and queries

--- app.py
+++ app.py
@@ -1,29 +1,57 @@
 from chalice import Chalice
 
+
 app = Chalice(app_name='chalice-app')
 
+from base import Session, engine, Base
+from models import User, UserTeams, Team
 
+session = Session()
+
+@app.route('/create_schema')
+def create_schema():
+    Base.metadata.create_all(engine)
+
+@app.route('/teams/{name}', methods=['POST'])
+def create_team(name):
+    session.add(Team(name))
+    return {"team": name}
+
+@app.route('/users/{team_id}', methods=['GET'])
+def list_users(team_id, sort=False):
+    if app.current_request.query_params['sort'] == "true":
+        sort = True
+    users = session.query(UserTeams) \
+    .filter(UserTeams.team_id == team_id) \
+    .all()
+    return users
+    #select team, users, position filter by position and active
+
+@app.route('/teams/{user_id}', methods=['GET'])
+def list_teams(user_id, sort=False):
+    if app.current_request.query_params['sort'] == "true":
+        sort = True
+    users = session.query(UserTeams) \
+    .filter(UserTeams.user_id == user_id) \
+    .all()
+    return users
+    #select users, teams sort by active, sort
+
+@app.route('/users/{email}', methods=['POST'])
+def create_user(email):
+    session.add(User(email=email))
+    return {'user': email}
+
+def team_add_member(team_id, user_id, position):
+    return True
+    # find team, user and create association
+
+def team_update_member(team_id, user_id, position, active):
+    return True
+    # find user, team update position and active
 @app.route('/')
 def index():
     return {'hello': 'world'}
 
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+session.commit()
+session.close()
--- models.py
+++ models.py
@@ -7,13 +7,13 @@ from base import Session, engine, Base
 
 class UserTeams(Base):
     __tablename__ = 'user_teams'
-    __table_args__ = (Index('idxteamposuser', team_id, user_id, position , unique=True) )
     team_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
     user_id = Column(Integer, ForeignKey('teams.id'), primary_key=True)
     position = Column(String(50))
     active = Column(Boolean, default=True)
-    child = relationship("Team", back_populates="users")
-    parent = relationship("User", back_populates="teams")
+    team = relationship("Team", back_populates="users")
+    user = relationship("User", back_populates="teams")
+    __table_args__ = (Index('idxteamposuser', team_id, user_id, position , unique=True) )
     
 
 
@@ -22,9 +22,8 @@ class Team(Base):
     id=Column(Integer, primary_key=True)
     name=Column('title', String(32))
     users = relationship(
-        "User",
-        secondary=user_teams,
-        back_populates="team")
+        "UserTeams",
+        back_populates="teams")
 
 
 class User(Base):
@@ -32,28 +31,5 @@ class User(Base):
     id=Column(Integer, primary_key=True)
     email=Column('title', String(120), unique=True)
     teams = relationship(
-        "Team",
-        secondary=user_teams,
+        "UserTeams",
         back_populates="users")
-
-
-session = Session()
-
-def create_team(name):
-    session.add(Team(name))
-
-def list_users(name, sort):
-    #select team, users, position filter by position and active
-
-def list_teams(email, sort):
-    #select users, teams sort by active, sort
-
-def create_user(email):
-    session.add(User(email=email))
-
-def team_add_member(team_id, user_id, position):
-    # find team, user and create association
-
-def team_update_member(team_id, user_id, position, active):
-    # find user, team update position and active
-
